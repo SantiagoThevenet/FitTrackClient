75ef9a6484504a902875b18732ff823e7b1a94a2
santiago <santithevenetvalles@gmail.com>
2023-09-13 19:03:47 +0200

    Test navbar with context

--- src/components/Navbar.test.jsx
+++ src/components/Navbar.test.jsx
@@ -1,17 +1,64 @@
 import { describe, expect, it } from "vitest";
-import { AuthProvider } from "../context/AuthContext";
+import { AuthContext, AuthProvider } from "../context/AuthContext";
 import TestRenderer from "react-test-renderer";
 import { MemoryRouter } from "react-router-dom";
 import Navbar from "./Navbar";
 
-
-
 describe("Navbar", () => {
-  it("should test the children inside my NavBar component", () => {
-    const navBar = TestRenderer.create(<AuthProvider><MemoryRouter><Navbar /></MemoryRouter></AuthProvider>).toJSON();
+  it("should test the children inside my NavBar component whes user is not authenticated", () => {
+    const navBar = TestRenderer.create(
+      <AuthProvider>
+        <MemoryRouter>
+          <Navbar />
+        </MemoryRouter>
+      </AuthProvider>
+    ).toJSON();
 
-    expect(navBar.children[0].props.href).toBe("/")
-    expect(navBar.children[1].children[0].children[0].props.href).toBe("/login")
-    expect(navBar.children[1].children[1].children[0].props.href).toBe("/register")
+    expect(navBar.children[0].props.href).toBe("/");
+    expect(navBar.children[1].children[0].children[0].props.href).toBe(
+      "/login"
+    );
+    expect(navBar.children[1].children[1].children[0].props.href).toBe(
+      "/register"
+    );
+  });  
+  
+  it("should test the children inside my NavBar component when user is authenticated", () => {
+    const isAuthenticated = true;
+    const user = {
+      username: "santi"
+    };
+    
+    const MockContextProvider = ({ children }) => {
+      const mockedValue = { isAuthenticated, user };
+      return (
+        <AuthContext.Provider value={mockedValue}>
+          {children}
+        </AuthContext.Provider>
+      );
+    };
+
+    const navBar = TestRenderer.create(
+      <AuthProvider>
+        <MemoryRouter>
+          <MockContextProvider>
+            <Navbar />
+          </MockContextProvider>
+        </MemoryRouter>
+      </AuthProvider>
+    ).toJSON();
+
+    // Realiza tus comprobaciones para verificar el comportamiento cuando isAuthenticated es true
+    expect(navBar.children[0].props.href).toBe("/");
+    console.log(navBar.children[1].children[2].children[0])
+    expect(navBar.children[1].children[2].children[0]).toBe(
+      "LOGOUT"
+    );
+    expect(navBar.children[1].children[1].children[0].props.href).toBe(
+      "/routines"
+    );
   });
 });
+
+
+ 
